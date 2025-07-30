522fea784eec40f911e0ffa4493b1e540de7fa3a
tinhdoo <hotinh13102@gmail.com>
2025-07-30 11:44:45 +0700

    up

--- src/ss18/ValidateEmail.java
+++ src/ss18/ValidateEmail.java
@@ -1,4 +1,23 @@
 package ss18;
 
+
 public class ValidateEmail {
+    static String listMail = "1nguyenvana@gmail.com;tranthib123@yahoo.com;lehoanganh@fpt.edu.vn;phamminhduc@outlook.com;ngoclinh2002@gmail.com";
+    static String[] arrs = listMail.split(";");
+
+    public static void main(String[] args) {
+        for (String arr : arrs) {
+            String email = arr.trim();
+            if (validateMail(email)) {
+                System.out.println(email + "true");
+            } else {
+                System.out.println(email + "false");
+            }
+        }
+    }
+    public static boolean validateMail(String mail) {
+        String regex = "^[A-Za-z][A-Za-z0-9_]{6,32}@[A-Za-z]+(\\.[A-Za-z]{2,12})$";
+        return mail.matches(regex);
+    }
+
 }
